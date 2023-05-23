d22c72ae05e642704669b9595aff86893de611ce
liupeng <peng.liu@aminer.cn>
2023-05-23 10:20:53 +0800

    feat：模版更新

--- src/component/LeftList/index.tsx
+++
@@ -1,9 +0,0 @@
-import react, { useState } from "react";
-import styles from "./index.less";
-interface ILeftList {}
-
-function LeftList(props: ILeftList) {
-  return <div className={styles.leftList}></div>;
-}
-
-export default LeftList;
